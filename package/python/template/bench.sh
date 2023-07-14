fc1aad229bdba714ad88d34f9c9ac281027d521b
Konstantin Demin <rockdrilla@gmail.com>
2023-07-14 09:00:18 +0300

    {image,package}/python: update
    Signed-off-by: Konstantin Demin <rockdrilla@gmail.com>

--- package/python/template/bench.sh
+++ package/python/template/bench.sh
@@ -2,16 +2,30 @@
 
 set -f
 
-PGO_EXCLUDE='test_dbm test_dbm_ndbm test_distutils test_ensurepip test_gdb test_ioctl test_lib2to3 test_minidom test_pyexpat test_selectors test_tools test_venv'
+PGO_EXCLUDE='test___all__ test_dbm test_dbm_ndbm test_devpoll test_distutils test_ensurepip test_gdb test_idle test_ioctl test_kqueue test_launcher test_lib2to3 test_linuxaudiodev test_minidom test_msilib test_ossaudiodev test_pyexpat test_selectors test_smtpnet test_socketserver test_startfile test_tcl test_tix test_tk test_tools test_ttk_guionly test_turtle test_urllib2net test_urllibnet test_venv test_winconsoleio test_winreg test_winsound test_xmlrpc_net test_zipfile64'
 PGO_RESOURCES='all,-audio,-gui,-network,-urlfetch'
-PGO_OPTS="--use=${PGO_RESOURCES} --exclude ${PGO_EXCLUDE}"
 
+PGO_OPTS=''
 case "${DEB_PGO_LEVEL}" in
 0) ;;
-*) PGO_OPTS="--pgo-extended ${PGO_OPTS}" ;;
+*) PGO_OPTS="--pgo-extended --use=${PGO_RESOURCES} --exclude ${PGO_EXCLUDE}" ;;
 esac
 
 unset CONTAINER_PYTHON_COMPAT
+
+flush_pycache() {
+	find "${DEB_SRC_TOPDIR:-/}" -name __pycache__ -type d -exec rm -rf '{}' '+'
+	find "${DEB_SRC_TOPDIR:-/}" -name '*.py[co]' -ls -delete
+}
+
+end_bench() {
+	[ -z "${DEB_SRC_TOPDIR}" ] || \
+	find "${DEB_SRC_TOPDIR}" -name '*.gcda' -exec ls -dlt {} +
+
+	exit 0
+}
+
+flush_pycache
 
 # run standard Python tests
 (
@@ -21,16 +35,18 @@ unset CONTAINER_PYTHON_COMPAT
 )
 
 case "${DEB_PGO_LEVEL}" in
-0|1) exit 0 ;;
+0|1) end_bench ;;
 esac
+
+flush_pycache
 
 python_bin=$(readlink -f "$1")
 python_wrap=$(readlink -f ./runpython.sh)
 
 do_pyperf() { "${python_wrap}" -m pyperformance "$@" ; }
 
-do_pyperf run --debug-single-value --python "${python_bin}"
-if [ "${DEB_PGO_LEVEL}" = 2 ] ; then exit 0 ; fi
+do_pyperf run --debug-single-value --python "${python_bin}" --benchmarks='-asyncio_tcp,-asyncio_tcp_ssl,-sqlalchemy_imperative'
+if [ "${DEB_PGO_LEVEL}" = 2 ] ; then end_bench ; fi
 
 do_asv() { "${python_wrap}" -m asv "$@" ; }
 do_asv_at() {
@@ -47,13 +63,14 @@ do_asv_at() {
 }
 
 do_asv_at ../pip-numpy/benchmarks
-if [ "${DEB_PGO_LEVEL}" = 3 ] ; then exit 0 ; fi
+if [ "${DEB_PGO_LEVEL}" = 3 ] ; then end_bench ; fi
 
 do_asv_at ../pip-pandas/asv_bench
-if [ "${DEB_PGO_LEVEL}" = 4 ] ; then exit 0 ; fi
+if [ "${DEB_PGO_LEVEL}" = 4 ] ; then end_bench ; fi
 
-do_asv_at ../pip-dask-benchmarks/dask
-do_asv_at ../pip-dask-benchmarks/distributed
-if [ "${DEB_PGO_LEVEL}" = 5 ] ; then exit 0 ; fi
+# dask benchmarks are kinda stalled
+# do_asv_at ../pip-dask-benchmarks/dask
+# do_asv_at ../pip-dask-benchmarks/distributed
+# if [ "${DEB_PGO_LEVEL}" = 5 ] ; then end_bench ; fi
 
-exit 0
+end_bench
