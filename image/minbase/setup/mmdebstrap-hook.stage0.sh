cc09c9f04d5659cf837490ec34488279f9a8f161
Konstantin Demin <rockdrilla@gmail.com>
2023-08-19 02:46:50 +0300

    image/minbase: CA certificates tasks
    - adjust preseed certificates (if any)
    - compare system CA bundle against python-certifi, detect missing entries and add them
    
    Signed-off-by: Konstantin Demin <rockdrilla@gmail.com>

--- image/minbase/setup/mmdebstrap-hook.stage0.sh
+++ image/minbase/setup/mmdebstrap-hook.stage0.sh
@@ -68,6 +68,10 @@ renmov() {
 	mv "$1" "$2"
 }
 
+rand4() {
+	od -v -A n -t x4 -N 4 < /dev/urandom | tr -d '[:space:]'
+}
+
 # fix ownership:
 # mmdebstrap's actions 'sync-in' and 'copy-in' preserves source user/group
 fix_ownership() {
@@ -84,6 +88,8 @@ find /usr/local/bin/ -type f -exec chmod 0755 {} +
 renmov /etc/apt/apt.conf.d/99mmdebstrap  /etc/apt/apt.conf.d/k2
 renmov /etc/dpkg/dpkg.cfg.d/99mmdebstrap /etc/dpkg/dpkg.cfg.d/k2
 
+apt-install openssl
+
 preseed='/usr/local/preseed'
 if [ -d "${preseed}" ] ; then
 	# CA certificates
@@ -92,9 +98,26 @@ if [ -d "${preseed}" ] ; then
 		d='/usr/local/share/ca-certificates'
 		mkdir -p "$d"
 
-		# rename *.pem -> *.crt (if any)
+		# convert/enforce *.pem (if any) to PEM format
 		find "$s/" -iname '*.pem' -type f \
-		  -execdir mv -vn '{}' '{}.crt' ';'
+		| while read -r src ; do
+			[ -n "${src}" ] || continue
+			dst_name="${src%.[Pp][Ee][Mm]}"
+			dst="${dst_name}.crt"
+			! [ -f "${dst}" ] || dst="${dst_name}.pem.crt"
+			! [ -f "${dst}" ] || dst="${dst_name}.$(rand4).pem.crt"
+			openssl x509 -in "${src}" -out "${dst}" -outform PEM
+			echo "${src} -> ${dst}"
+			rm -f "${src}"
+		done
+
+		# adjust file name extensions
+		find "$s/" -iname '*.crt' -type f \
+		| while read -r src ; do
+			[ -n "${src}" ] || continue
+			dst="${src%.[Cc][Rr][Tt]}.crt"
+			[ "${src}" = "${dst}" ] || mv -fv "${src}" "${dst}"
+		done
 
 		# copy *.crt (with directory structure, if any)
 		find "$s/" -name '*.crt' -type f -printf '%P\0' \
@@ -123,6 +146,80 @@ mkdir -p /etc/k2/dpkg-filter/
 /usr/local/lib/k2/bootstrap/settings.sh
 
 # generate CA bundles
+update-ca-certificates --fresh
+
+# adjust missing CAs with certifi
+apt-install curl
+
+certifi_uri="${MMDEBSTRAP_MIRROR_CERTIFI:-https://github.com/certifi/python-certifi/raw/master}/certifi/cacert.pem"
+{
+	w=$(mktemp -d) ; : "${w:?}"
+	curl -sSL -o "$w/cacert.pem" "${certifi_uri}"
+
+	def_bundle='/etc/ssl/certs/ca-certificates.crt'
+
+	bundle_offsets() {
+		grep -Fhne '-----END CERTIFICATE-----' "$1" | cut -d : -f 1 \
+		| {
+			s=1 ; while read -r e ; do
+				[ -n "$e" ] || continue
+				echo "$s,$e"
+				s=$((e+1))
+			done
+		}
+	}
+
+	set +e
+	bundle_offsets "${def_bundle}" > "$w/offsets.0"
+	bundle_offsets "$w/cacert.pem" > "$w/offsets.1"
+	set -e
+
+	bundle_fingerprints() {
+		while read -r a ; do
+			[ -n "$a" ] || continue
+			sed -ne "${a}p" "$1" | openssl x509 -noout -fingerprint
+		done < "$2"
+	}
+
+	set +e
+	bundle_fingerprints "${def_bundle}" "$w/offsets.0" > "$w/fingerprints.0"
+	bundle_fingerprints "$w/cacert.pem" "$w/offsets.1" > "$w/fingerprints.1"
+	set -e
+
+	set +e
+	grep -Fxv -f "$w/fingerprints.0" "$w/fingerprints.1" > "$w/fingerprints.diff"
+	set -e
+
+	if [ -s "$w/fingerprints.diff" ] ; then
+		set +e
+		grep -Fxn -f "$w/fingerprints.diff" "$w/fingerprints.1" | cut -d : -f 1 > "$w/records.diff"
+		set -e
+
+		terse_fingerprint() {
+			cut -d = -f 2- | tr '[:upper:]' '[:lower:]' | tr -cd '[:alnum:]'
+		}
+
+		mkdir "$w/certifi-extras"
+
+		while read -r n ; do
+			[ -n "$n" ] || continue
+			fp=$(sed -ne "${n}p" "$w/fingerprints.1" | terse_fingerprint)
+			off=$(sed -ne "${n}p" "$w/offsets.1")
+			sed -ne "${off}p" "$w/cacert.pem" | openssl x509 > "$w/certifi-extras/${fp}.crt"
+		done < "$w/records.diff"
+
+		env -C "$w" find "certifi-extras/" -mindepth 1 -exec ls -ld {} +
+
+		for d in /usr/local/preseed/crt /usr/local/share/ca-certificates ; do
+			mkdir -p "$d"
+			tar -C "$w" -cf - certifi-extras | tar -C "$d" -xf -
+		done
+	fi
+
+	rm -rf "$w"
+}
+
+# generate CA bundles (again)
 update-ca-certificates --fresh
 
 # cleanup
